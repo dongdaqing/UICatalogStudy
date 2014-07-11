6c75a55b4c44e2fc5bc89bd628ba79f06d126f7e
dongdaqing <ddaqing@gmail.com>
2014-07-11 18:07:30 +0800

    first commit

---
+++ UICatalogStudy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UICatalogStudy
+//
+//  Created by DongDaqing on 14-7-11.
+//  Copyright (c) 2014年 DongDaqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
